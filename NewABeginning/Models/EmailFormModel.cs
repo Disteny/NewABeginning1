52d05f5dac08427a31290a724cfe0b7bf1998c91
nabin <nabin.adhikari1123@outlook.com>
2017-07-31 19:24:25 -0500

    Email update and about update

---
+++ NewABeginning/Models/EmailFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewABeginning.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your Full Name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your EmailAddress"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required, Display(Name = "Subject")]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public HttpPostedFileBase Upload { get; set; }
+    }
+}
